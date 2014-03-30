a1bb80b50e17e0b8a1d9ced13d1403ed38944e39
scherrda <dahlia.scherr@gmail.com>
2014-03-31 01:03:17 +0200

    Item upgrade

--- src/main/java/fr/xebia/xebay/dto/Item.java
+++ src/main/java/fr/xebia/xebay/dto/Item.java
@@ -6,26 +6,37 @@ public class Item {
     private String name;
     private String category;
     private double value;
+    private boolean offered;
 
     public Item() {
-    }
 
-    public Item(String category, String name, double value) {
-        this.name = name;
+    }
+    
+    public Item(String category, String name, double initialValue, boolean offered) {
         this.category = category;
-        this.value = value;
+        this.name = name;
+        this.value = initialValue;
+        this.offered = offered;
     }
 
-    public String getCategory() {
-        return category;
+    public Item(String category, String name, double initialiValue) {
+        this(category, name, initialiValue, false);
     }
 
     public String getName() {
         return name;
     }
 
+    public String getCategory() {
+        return category;
+    }
+
     public double getValue() {
         return value;
+    }
+
+    public boolean isOffered() {
+        return offered;
     }
 
     @Override
